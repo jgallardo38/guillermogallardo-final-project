d8a9b2175cb0d22b614278cd7a48133512bfbae0
jgallardo38 <jgallardo38@gmail.com>
2024-07-10 19:00:19 -0500

    Project part 3 - statistical analysis

--- code/analysis-code/statistical-analysis.R
+++ code/analysis-code/statistical-analysis.R
@@ -10,57 +10,152 @@ library(broom) #for cleaning up output from lm()
 library(here) #for data loading/saving
 library(tidyverse)
 library(tidymodels)
+library(caret) # For modeling and evaluation
+library(e1071)  
+library(rsample)
+library(parsnip)
 
-update.packages(ask = FALSE)
-
-#path to data
-#note the use of the here() package and not absolute paths
+####################################
+# Loading Data
 data_location = here::here("data","processed-data","HRprocesseddata.rds")
+hr_data = readRDS(data_location)
 
-#load data. 
-mydata = readRDS(data_location)
-
-
-######################################
-#Data fitting/statistical analysis
-######################################
-
-#### Linear Model Job Satisfaction ~ Department
-
-#lmfitHR = lm(JobSatisfaction~Age, data = mydata)
+####################################
 
 
+#Bivariate
 
+#####################################
+## Age Logistic Regression
+age_glm = glm(Attrition ~ Age, data = hr_data, family = binomial())
+summary(age_glm)
 
-############################
-#### First model fit
-# fit linear model using height as outcome, weight as predictor
-
-lmfit1 <- lm(Height ~ Weight, mydata)  
-
-# place results from fit into a data frame with the tidy function
-lmtable1 <- broom::tidy(lmfit1)
-
+## Age Export table
+###tidy function
+lgAge = broom::tidy(age_glm)
+lgAge$p.value <- format(lgAge$p.value, digits = 6)
 #look at fit results
-print(lmtable1)
-
+print(lgAge)
 # save fit results table  
-table_file1 = here("results", "tables", "resulttable1.rds")
-saveRDS(lmtable1, file = table_file1)
+table_lgAge = here("results", "tables", "logistic_age.rds")
+saveRDS(lgAge, file = table_lgAge)
+####################################
 
-############################
-#### Second model fit
-# fit linear model using height as outcome, weight and gender as predictor
 
-lmfit2 <- lm(Height ~ Weight + Gender, mydata)  
+####################################
+## Income Logistic Regression
+inc_glm = glm(Attrition ~ MonthlyIncome, data = hr_data, family = binomial())
+summary(inc_glm)
 
-# place results from fit into a data frame with the tidy function
-lmtable2 <- broom::tidy(lmfit2)
-
+## Income Export table
+###tidy function
+lgInc = broom::tidy(inc_glm)
+lgInc$p.value <- format(lgInc$p.value, digits = 6)
 #look at fit results
-print(lmtable2)
-
+print(lgInc)
 # save fit results table  
-table_file2 = here("results", "tables", "resulttable2.rds")
-saveRDS(lmtable2, file = table_file2)
+table_lgInc = here("results", "tables", "logistic_income.rds")
+saveRDS(lgInc, file = table_lgInc)
+####################################
 
+
+####################################
+## Chi-squared
+education_table <- table(hr_data$Attrition, hr_data$EducationField)
+chisq_test <- chisq.test(education_table)
+chisq_test
+
+####################################
+
+
+####################################
+# Multivariable Analysis
+
+hr_data$Attrition = ifelse(hr_data$Attrition == "Yes", 1, 0)
+glm_model <- glm(Attrition ~ Age + MonthlyIncome + JobSatisfaction, data = hr_data, family = binomial())
+
+# Summary of the model
+summary(glm_model)
+
+## Income Export table
+###tidy function
+glmAMJ = broom::tidy(glm_model)
+glmAMJ$p.value <- format(glmAMJ$p.value, digits = 6)
+#look at fit results
+print(glmAMJ)
+# save fit results table  
+table_GLM = here("results", "tables", "glm_model.rds")
+saveRDS(glmAMJ, file = table_GLM)
+
+####################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################
+##IN PROGRESS FOR PART 4
+
+set.seed(42)
+
+hr_data$Attrition <- factor(hr_data$Attrition, levels = c("No", "Yes"))
+table(hr_data$Attrition)
+
+# Ensure all categorical variables are factors and check initial levels
+categorical_cols <- sapply(hr_data, is.character)
+hr_data[categorical_cols] <- lapply(hr_data[categorical_cols], factor)
+
+# Splitting data with stratification
+split <- createDataPartition(y = hr_data$Attrition, p = 0.7, list = FALSE, times = 1)
+training_set <- hr_data[split, ]
+testing_set <- hr_data[-split, ]
+
+
+# Check and adjust factor levels in training set
+training_set <- training_set %>%
+  mutate_if(is.factor, function(x) {
+    if (length(levels(x)) < 2) {
+      x <- NULL  # Drop the variable if only one level
+    } else {
+      x
+    }
+  })
+
+
+# Pre-processing data
+preproc <- preProcess(training_set, method = c("center", "scale"))
+train_processed <- predict(preproc, training_set)
+test_processed <- predict(preproc, testing_set)
+
+
+########## Random Forest Model
+fit_rf <- train(Attrition ~ ., data = train_processed, method = "rf", 
+                trControl = trainControl(method = "cv", number = 10))
+
+# Evaluating the Random Forest Model
+rf_predictions <- predict(fit_rf, newdata = test_processed)
+rf_conf_matrix <- confusionMatrix(rf_predictions, testing_set$Attrition)
+print(rf_conf_matrix)
+
+
+importance <- varImp(fit_rf, scale = FALSE)
+print(importance)
+
+hr_matrix = here("results", "tables", "hr_matrix.rds")
+saveRDS(rf_conf_matrix, file = hr_matrix)
+
+var_importance = here("results", "tables", "importance.rds")
+saveRDS(importance, file = var_importance)
+##########################################
